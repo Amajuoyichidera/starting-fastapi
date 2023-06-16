d03c88b87655ac1a79182f7e8db4356246b6da93
Amajuoyi chidera <amajuoyichideradavid@gmail.com>
2023-06-16 17:28:40 +0100

    first version

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from typing import Optional
+
+app = FastAPI()
+
+@app.get('/blog')
+def index(limit=10,published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else :
+        return {'data': f'{limit} blogs from the db'}
+    
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'all unpublished'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    # fetch blog with id = id
+    return {'data':id}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    # fetch comments of blog with id = id
+    return {'data': {'1', '2'}}
+
